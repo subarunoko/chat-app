bb44d3fa509c19f0582ce802a562a7dbe55279aa
subarunoko <kazenonakasubaru@yahoo.co.jp>
2020-12-24 00:12:13 +0900

    画像表示の実装 他

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,6 +20,7 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    # params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 end
